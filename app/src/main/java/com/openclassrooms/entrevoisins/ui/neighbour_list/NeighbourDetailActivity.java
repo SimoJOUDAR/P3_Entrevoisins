e7bcf6927e6c25f692f3b0290ee8f57c3bbffc10
SimoJOUDAR <ssjoudar@gmail.com>
2022-04-13 16:53:05 +0200

    NeighbourDetailActivity : Save Favorite Neighbour - Update Favorite_Button

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailActivity.java
@@ -1,7 +1,11 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,10 +20,14 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.openclassrooms.entrevoisins.R;
 
+import java.util.HashMap;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class NeighbourDetailActivity extends AppCompatActivity {
+
+    public static final String FAVORITES = "Favorites" ;
 
     public static final String EXTRA_USER_ID = "com.openclassrooms.entrevoisins.ui.neighbour_detail.NeighbourDetailActivity.EXTRA_USER_ID";
     public static final String EXTRA_USER_AVATAR = "com.openclassrooms.entrevoisins.ui.neighbour_detail.NeighbourDetailActivity.EXTRA_USER_AVATAR";
@@ -37,6 +45,9 @@ public class NeighbourDetailActivity extends AppCompatActivity {
     @BindView(R.id.detail_about) TextView about;
     @BindView(R.id.detail_favorite_button) ImageButton favoriteButton;
 
+    SharedPreferences sharedpreferences;
+    String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,12 +55,20 @@ public class NeighbourDetailActivity extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setContentView(R.layout.activity_neighbour_detail);
         ButterKnife.bind(this);
+        userId = getIntent().getStringExtra(EXTRA_USER_ID);
+        sharedpreferences = getSharedPreferences(FAVORITES, Context.MODE_PRIVATE);
         this.updateUI();
+        updateFavoriteButton();
 
         favoriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //addToFavorite(id);
+               if (isFavorite(userId)){
+                   removeFromFavorite(userId);
+               }
+               else {
+                   addToFavorite(userId);
+               }
             }
         });
     }
@@ -64,8 +83,31 @@ public class NeighbourDetailActivity extends AppCompatActivity {
         this.about.setText(getIntent().getStringExtra(EXTRA_USER_ABOUT));
     }
 
-    private void addToFavorite(String id) {
-        String userId = getIntent().getStringExtra(EXTRA_USER_ID);
+    private boolean isFavorite(String userId) {
+        return sharedpreferences.contains(userId);
+    }
+
+    public void removeFromFavorite(String userId){
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.remove(userId);
+        editor.apply();
+        updateFavoriteButton();
+    }
+
+    public void addToFavorite(String userId){
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(userId, userId);
+        editor.apply();
+        updateFavoriteButton();
+    }
+
+    public void updateFavoriteButton(){
+        if (isFavorite(userId)){
+            favoriteButton.setImageResource(R.drawable.ic_star_white_24dp);
+        }
+        else {
+            favoriteButton.setImageResource(R.drawable.ic_star_border_white_24dp);
+        }
     }
 
 }
